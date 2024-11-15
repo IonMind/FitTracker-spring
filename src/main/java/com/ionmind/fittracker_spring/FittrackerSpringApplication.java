c6f97a9773a1d1af3c451940ad642043f2440120
Abhishek <kumarakc55@gmail.com>
2024-11-15 10:00:00 +0530

    Initial Commit with skeleton

---
+++ src/main/java/com/ionmind/fittracker_spring/FittrackerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ionmind.fittracker_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittrackerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittrackerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ionmind/fittracker_spring/FittrackerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionmind.fittracker_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittrackerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
